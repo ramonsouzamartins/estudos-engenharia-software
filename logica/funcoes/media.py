27c5ba10503be57282ccbe6ab627ec88dd0409cd
RAMON SOUZA MARTINS <sramonmartins@gmail.com>
2026-01-07 13:53:59 -0300

    Adiciona função para cálculo de média em Python

---
+++ logica/funcoes/media.py
@@ -0,0 +1,10 @@
+def calcular_media(nota1, nota2):
+    """
+    Calcula a média entre duas notas
+    """
+    media = (nota1 + nota2) / 2
+    return media
+
+
+resultado = calcular_media(7, 9)
+print("A média é:", resultado)
